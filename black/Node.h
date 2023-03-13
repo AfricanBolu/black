b72bca44330bd4b39e7c80a4efb7f2e2fc54d73e
gabe-hi <gabez14z14@gmail.com>
2023-03-13 02:10:44 -0500

    node classes

---
+++ black/Node.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+
+};
+
+class HeaderNode : public Node
+{
+public:
+};
+
+class InternalNode : public Node
+{
+public:
+};
