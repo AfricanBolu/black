36cdcbb48436a4d46516e5cb598bcc9c6baf4d3a
gabe-hi <gabez14z14@gmail.com>
2023-03-13 02:42:42 -0500

    node classes with pointers

--- black/Node.h
+++ black/Node.h
@@ -5,16 +5,30 @@ using namespace std;
 
 class Node
 {
-public:
+protected:
+    int row;
+    int col;
+    int value;
 
+public:
 };
 
 class HeaderNode : public Node
 {
+private:
+    Node *right;
+    Node *down;
+
 public:
 };
 
 class InternalNode : public Node
 {
+private:
+    Node *left;
+    Node *right;
+    Node *up;
+    Node *down;
+
 public:
 };
